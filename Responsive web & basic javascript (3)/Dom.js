f19b850d25b9b3b7aebb6b61bf7d1ec2f3797c5f
yashika4332 <mandhanyashika2@gmail.com>
2026-09-03 14:21:39 -0700

    Add files via upload

---
+++ Responsive web & basic javascript (3)/Dom.js
@@ -0,0 +1,9 @@
+let heading = document.getElementById("title");
+let paragraph = document.getElementById("message");
+let button=document.getElementById("showBtn");
+button.onclick=function(){
+    alert(heading.textContent);
+
+};
+ console.log(paragraph.textContent);
+ document.write("This is my first time writing javascript");
---
+++ Responsive web & basic javascript (3)/datatype.js
@@ -0,0 +1,31 @@
+// Using let with string datatype
+let name = "Yashika";
+console.log("",typeof(name),"",name);
+// using var with number datatype
+var Num = 20299988;
+console.log("",typeof(Num),"",Num);
+// using const with boolean datatype
+const status = true;
+console.log("",typeof(status),"",status);
+//using let but undefined datatype
+let a;
+console.log("",typeof(a),"",a);
+// using const with object datatype
+const obj = {name:"Aastha"};
+console.log("",typeof(obj)," ",obj);
+// using let with array datatype
+let arr = ["A","B","c","D"];
+console.log("",typeof(arr),"",arr);
+// using let with null datatype
+let box=null;
+console.log("",typeof(box),"",box);
+//using let with symbol datatype
+let e = Symbol("Id");
+console.log(e);
+console.log(typeof(e));
+// using var with big number datatype
+var bignum=1234567890787654321233456677n;
+console.log(bignum);
+console.log(typeof(bignum));
+
+
